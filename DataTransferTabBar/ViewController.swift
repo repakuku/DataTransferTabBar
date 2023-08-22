c415a0429b2680e7bc59da9ae6872af105c8f8c4
Alexey Turulin <repakuku@icloud.com>
2023-08-22 12:45:00 -0400

    Initial Commit

---
+++ DataTransferTabBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DataTransferTabBar
+//
+//  Created by Alexey Turulin on 8/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
