7d866a0459392d7310eb865c5ef12d59bef5f473
Alexey Turulin <repakuku@icloud.com>
2023-08-22 13:23:06 -0400

    working with ui

---
+++ DataTransferTabBar/ThirdViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ThirdViewController.swift
+//  DataTransferTabBar
+//
+//  Created by Alexey Turulin on 8/22/23.
+//
+
+import UIKit
+
+final class ThirdViewController: UIViewController {
+
+}
